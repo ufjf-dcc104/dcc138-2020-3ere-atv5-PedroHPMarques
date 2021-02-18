59275ee15bc25633a262752c9f60a276683cde77
Pedro H P Marques <pmarques@ice.ufjf.br>
2021-02-18 13:15:14 -0300

    cria e usa uma classe de cena

---
+++ js/Cena.js
@@ -0,0 +1,11 @@
+export default class Cena {
+    // É responsavel por desenhar elementos na tela
+    constructor(canvas){
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+    }
+    desenhar(){
+        this.fillStyle = "grey";
+        this.ctx.fillRect(0 ,0 , this.canvas.width, this.canvas.height);
+    }
+}

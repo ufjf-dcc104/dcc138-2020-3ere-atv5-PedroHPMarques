b635092aece8f14ad3f041bacf8b2877b4890730
Pedro H P Marques <pmarques@ice.ufjf.br>
2021-02-18 16:00:16 -0300

    Marca sprites colididos para remoçao

--- js/Cena.js
+++ js/Cena.js
@@ -4,6 +4,7 @@ export default class Cena {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
         this.sprites = [];
+        this.aRemover = [];
         this.t0 = 0
         this.dt = 0
         this.idAnim = null;
@@ -51,10 +52,20 @@ this.idAnim = requestAnimationFrame(
             const spriteA = this.sprites[a];
             for (let b = a + 1; b < this.sprites.length; b++) {
                 const spriteB = this.sprites[b];
-                
+                if(spriteA.colidiuCom(spriteB)){
+                    this.quandoColidir(spriteA,spriteB);
+                }
             }
 
             
         }
+    }
+    quandoColidir(a,b){
+        if(! this.aRemover.includes(a)){
+            this.aRemover.push(a);
+            }
+            if(! this.aRemover.includes(b)){
+                this.aRemover.push(b);
+                }
     }
 }

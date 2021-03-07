d85640e794276f247253f2e9f432e0004af364aa
Pedro H P Marques <pmarques@ice.ufjf.br>
2021-03-07 14:29:15 -0300

    ex de tile

--- js/Mapa.js
+++ js/Mapa.js
@@ -24,6 +24,13 @@ export default class Mapa {
             ctx.strokeStyle = "black";
             ctx.strokeRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
             break;
+            case 2:
+            ctx.fillStyle = "red";
+            ctx.fillRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "orange";
+            ctx.strokeRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
+            break;
           default:
             ctx.fillStyle = "black";
             ctx.fillRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
@@ -33,5 +40,18 @@ export default class Mapa {
         }
       }
     }
+  }
+
+  carregaMapa(modelo){
+      this.LINHAS = modelo.length;
+      this.COLUNAS = modelo[0]?.length ?? 0;
+
+      this.tile = [];
+      for(let l = 0; l < this.LINHAS; l++){
+          this.tiles[l] = [];
+          for(let c = 0; c < this.COLUNAS; c++){
+              this.tiles[l][c] = modelo [l][c];
+          }
+      }
   }
 }
